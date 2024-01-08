dc36cd1b824682db4ce79b871929d0fbb1b46c65
Blankeos <carloantonioct@gmail.com>
2024-01-09 00:46:59 +0800

    feat: finished prototype. Learned stuff 🥳✨.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.pcss';
+</script>
+
+<svelte:head>
+	<!-- <meta name="viewport" content="width=device-width, maximum-scale=1" /> -->
+</svelte:head>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	/** @ts-ignore */
+	import Flippable from 'svelte-flip';
+	let flip = false;
+
+	let innerHeight: number;
+
+	let PADDING_Y = 100;
+
+	$: cardHeight = innerHeight - PADDING_Y;
+</script>
+
+<svelte:window bind:innerHeight />
+
+<div class="grid h-screen place-items-center overflow-hidden">
+	<!-- <h1>Welcome to SvelteKit</h1>
+
+	<button on:click={() => (flip = !flip)}>Flip!</button> -->
+	<div class="absolute top-10 z-20 mx-auto animate-bounce">
+		<span class="select-none">Tap to Flip</span>
+		<span></span>
+	</div>
+	<Flippable height={`${cardHeight}px`} width={`${cardHeight / 1.41}px`} {flip}>
+		<button class="select-none" slot="front" on:click={() => (flip = !flip)}>
+			<img src="/front.png" alt="front" />
+		</button>
+		<button class="select-none" slot="back" on:click={() => (flip = !flip)}>
+			<img src="/back.png" alt="back" />
+		</button>
+	</Flippable>
+</div>
